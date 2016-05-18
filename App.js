80ca20e8b1e665e0179812dce41cd621486a9cbc
hoagies <sphogan85@gmail.com>
2016-05-18 19:48:22 +0000

    updating with grid

--- App.js
+++ App.js
@@ -4,6 +4,31 @@ Ext.define('CustomApp', {
     launch: function() {
         //Write app code here
 
+        this.add({
+            xtype: 'rallygrid',
+            columnCfgs: [
+                'FormattedID',
+                'Name',
+                'State',
+                'ToDo'
+            ],
+            context: this.getContext(),
+            features: [{
+                ftype: 'groupingsummary',
+                groupHeaderTpl: '{name} ({rows.length})'
+            }],
+            storeConfig: {
+                model: 'task',
+                groupField: 'Owner',
+                groupDir: 'ASC',
+                fetch: ['Owner'],
+                getGroupString: function(record) {
+                    var owner = record.get('Owner');
+                    return (owner && owner._refObjectName) || 'No Owner';
+                }
+            }
+        });
+
         //API Docs: https://help.rallydev.com/apps/2.1/doc/
     }
 });
