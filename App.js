1fe62cde0984fed1fe8a9e46a3a6c0eb65238c0f
hoagies <sphogan85@gmail.com>
2016-05-19 04:18:41 +0000

    removing all

--- App.js
+++ App.js
@@ -2,114 +2,161 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     
-    
     launch: function() {
-        var filters = [], timeboxScope = this.getContext().getTimeboxScope();
+
+        var filters = [];
+        var timeboxScope = this.getContext().getTimeboxScope();
+
         if(timeboxScope) {
             filters.push(timeboxScope.getQueryFilter());
         }
 
-        this.board = this.add({
-            xtype: 'rallygrid',
-            columnCfgs: [
-                'FormattedID',
-                'Name',
-                // 'State',
-                'Iteration',
-                // 'ToDo',
-                // 'c_TeamMembers'
-            ],
-            // context: this.getContext(),
-            // features: [{
-            //     ftype: 'groupingsummary',
-            //     groupHeaderTpl: '{name} ({rows.length})'
-            // }],
-            storeConfig: {
-                model: 'userstory',
-            //     groupField: 'c_TeamMembers',
-            //     groupDir: 'ASC',
-            //     fetch: ['c_TeamMembers'],
-                filters: filters,
-            //     getGroupString: function(record) {
-            //         var owner = record.get('c_TeamMembers');
-            //         return (owner) || 'No Team Member';
-            //     }
-            }
-        });
+        this.getFilteredStoryModel(filters);            
     },
 
-    onTimeboxScopeChange: function(newTimeboxScope) {
-        this.callParent(arguments);
+    onTimeboxScopeChange: function(newTimeboxScope) {               
+        var newFilters = [];
+        var updatedTimeboxScope = this.getContext().getTimeboxScope();
+        if (this.grid) {
+            this.grid.destroy();
+        }                   
+        if (updatedTimeboxScope) {
+            newFilters.push(newTimeboxScope.getQueryFilter());
+        }
+        this.getFilteredStoryModel(newFilters);
+    },
 
-        this.board.refresh({
-            storeConfig: {
-                filters: [
-                    newTimeboxScope.getQueryFilter()
-                ]
-            }
-        });
+    getFilteredStoryModel: function(queryFilters) {
+        Rally.data.ModelFactory.getModel({
+            type: 'UserStory',
+            success: function(model) {
+                this.grid = this.add({
+                    xtype: 'rallygrid',
+                    model: model,
+                    columnCfgs: [
+                        'FormattedID',
+                        'Name',
+                        'Owner',
+                        'Iteration'
+                    ],
+                    storeConfig: {
+                        filters: queryFilters
+                    }
+                });
+            },
+            scope: this
+        });                 
     }
     
+    
     // launch: function() {
-    //     //Write app code here
-        
-    //     var timeboxScope = this.getContext().getTimeboxScope();
-    //     if(timeboxScope) {
-    //         var record = timeboxScope.getRecord();
-    //         var name = record.get('Name');
-    //         var startDate = timeboxScope.getType() === 'iteration' ? 
-    //             record.get('StartDate') : record.get('ReleaseStartDate');
-    //     }
-        
-    //     var filters = [];
-    //     // var timeboxScope = this.getContext().getTimeboxScope();
+    //     var filters = [], timeboxScope = this.getContext().getTimeboxScope();
     //     if(timeboxScope) {
     //         filters.push(timeboxScope.getQueryFilter());
     //     }
-    
-    //     this.grid = this.add({
-    //         xtype: 'rallygrid',
+
+    //     this.board = this.add({
+    //         xtype: 'rallygridboard',
+    //         modelNames: 'userstory',
     //         columnCfgs: [
     //             'FormattedID',
     //             'Name',
-    //             'State',
-    //             'ToDo',
-    //             'c_TeamMembers'
+    //             // 'State',
+    //             'Iteration',
+    //             // 'ToDo',
+    //             // 'c_TeamMembers'
     //         ],
-    //         context: this.getContext(),
-    //         features: [{
-    //             ftype: 'groupingsummary',
-    //             groupHeaderTpl: '{name} ({rows.length})'
-    //         }],
+    //         // context: this.getContext(),
+    //         // features: [{
+    //         //     ftype: 'groupingsummary',
+    //         //     groupHeaderTpl: '{name} ({rows.length})'
+    //         // }],
     //         storeConfig: {
-    //             model: 'task',
-    //             groupField: 'c_TeamMembers',
-    //             groupDir: 'ASC',
-    //             fetch: ['c_TeamMembers'],
+    //             model: 'userstory',
+    //         //     groupField: 'c_TeamMembers',
+    //         //     groupDir: 'ASC',
+    //         //     fetch: ['c_TeamMembers'],
     //             filters: filters,
-    //             getGroupString: function(record) {
-    //                 var owner = record.get('c_TeamMembers');
-    //                 return (owner) || 'No Team Member';
-    //             }
+    //         //     getGroupString: function(record) {
+    //         //         var owner = record.get('c_TeamMembers');
+    //         //         return (owner) || 'No Team Member';
+    //         //     }
     //         }
     //     });
     // },
 
     // onTimeboxScopeChange: function(newTimeboxScope) {
-    //     var grid = this.down('rallygrid');
     //     this.callParent(arguments);
-    //     console.log('newTimeboxScope: ',newTimeboxScope);
-    //     // console.log('board: ',this.board);
-    //     console.log('grid: ',grid);
-    //     // this.board.refresh({
-    //     grid.refresh({
+
+    //     this.board.refresh({
     //         storeConfig: {
     //             filters: [
     //                 newTimeboxScope.getQueryFilter()
     //             ]
     //         }
     //     });
-    // },
+    // }
+    
+    // // launch: function() {
+    // //     //Write app code here
+        
+    // //     var timeboxScope = this.getContext().getTimeboxScope();
+    // //     if(timeboxScope) {
+    // //         var record = timeboxScope.getRecord();
+    // //         var name = record.get('Name');
+    // //         var startDate = timeboxScope.getType() === 'iteration' ? 
+    // //             record.get('StartDate') : record.get('ReleaseStartDate');
+    // //     }
+        
+    // //     var filters = [];
+    // //     // var timeboxScope = this.getContext().getTimeboxScope();
+    // //     if(timeboxScope) {
+    // //         filters.push(timeboxScope.getQueryFilter());
+    // //     }
+    
+    // //     this.grid = this.add({
+    // //         xtype: 'rallygrid',
+    // //         columnCfgs: [
+    // //             'FormattedID',
+    // //             'Name',
+    // //             'State',
+    // //             'ToDo',
+    // //             'c_TeamMembers'
+    // //         ],
+    // //         context: this.getContext(),
+    // //         features: [{
+    // //             ftype: 'groupingsummary',
+    // //             groupHeaderTpl: '{name} ({rows.length})'
+    // //         }],
+    // //         storeConfig: {
+    // //             model: 'task',
+    // //             groupField: 'c_TeamMembers',
+    // //             groupDir: 'ASC',
+    // //             fetch: ['c_TeamMembers'],
+    // //             filters: filters,
+    // //             getGroupString: function(record) {
+    // //                 var owner = record.get('c_TeamMembers');
+    // //                 return (owner) || 'No Team Member';
+    // //             }
+    // //         }
+    // //     });
+    // // },
+
+    // // onTimeboxScopeChange: function(newTimeboxScope) {
+    // //     var grid = this.down('rallygrid');
+    // //     this.callParent(arguments);
+    // //     console.log('newTimeboxScope: ',newTimeboxScope);
+    // //     // console.log('board: ',this.board);
+    // //     console.log('grid: ',grid);
+    // //     // this.board.refresh({
+    // //     grid.refresh({
+    // //         storeConfig: {
+    // //             filters: [
+    // //                 newTimeboxScope.getQueryFilter()
+    // //             ]
+    // //         }
+    // //     });
+    // // },
     
     
 });
